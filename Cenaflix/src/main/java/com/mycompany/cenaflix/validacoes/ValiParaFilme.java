42f858f1bef48830ac7ed03fc2f18b4d51374540
Carlos Alexandre Torrecilhas da Silva Sá <carlos.sa@ameh.org.br>
2025-01-07 10:18:16 -0300

    Corrigimos o fluxo de validação das informações. Agora a validação está funcionando normalmente. Adicionamos também um classe controlador que chamada a validação dos dados e caso os mesmos estejam corretos, os valores são inseridos

--- Cenaflix/src/main/java/com/mycompany/cenaflix/validacoes/ValiParaFilme.java
+++ Cenaflix/src/main/java/com/mycompany/cenaflix/validacoes/ValiParaFilme.java
@@ -2,6 +2,8 @@ package com.mycompany.cenaflix.validacoes;
 
 //Pacotes do Projeto
 import com.mycompany.cenaflix.modelos.ParaFilmeRecord;
+//Pacotes Swing
+import javax.swing.JOptionPane;
 //Pacotes JAVA util
 import java.util.Map;
 import java.util.LinkedHashMap;
@@ -10,32 +12,31 @@ public class ValiParaFilme {
     
     private LinkedHashMap<String, String> validacao = new LinkedHashMap();
     private LinkedHashMap<String, String> valoresParaValidar = new LinkedHashMap();
-    private final String[] mensagemErro = {"Por favor, inserir o nome do filme, Por favor, verificar se a data segue esse modelo "
+    private final String[] mensagemErro = {"Por favor, inserir o nome do filme.", "Por favor verificar se a data segue esse modelo "
             + "dd/mm/aaaa", "Por favor, é necessário selecione um genero"};
     private ParaFilmeRecord parametroFilme;
-    
+    private int contator = 0;
     
     public ValiParaFilme(ParaFilmeRecord parametroFilme){
         this.parametroFilme = parametroFilme;
         this.setValiParaFilme();
-        this.setValoresParaValidar();
     }
     
     public boolean validarValores(){
+        this.setValoresParaValidar();
         for(Map.Entry<String, String> entry: this.valoresParaValidar.entrySet()){
             String chave = entry.getKey();
             String valorParaValidar = entry.getValue();
             
-            if(validacao.containsKey(chave)){
+            if(this.validacao.containsKey(chave)){
                 String validador = validacao.get(chave);
                 if(!valorParaValidar.matches(validador)){
-                    return false;
-                }
-                else{
+                    JOptionPane.showMessageDialog(null, this.mensagemErro[this.contator]);
                     return false;
                 }
             }
         }
+        this.contator = 0;
         return true;
     }
     
